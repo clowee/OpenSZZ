86e52ad79871fec59c8bdfc519d9d05b444661a4
Vladyslav Bondarenko <vladyslav.bondarenko@ut.ee>
2021-05-29 02:06:21 +0300

    analyze any files with an extension other than .txt and .md instead of analyzing only .java files

--- src/com/SZZ/jiraAnalyser/entities/Link.java
+++ src/com/SZZ/jiraAnalyser/entities/Link.java
@@ -244,6 +244,12 @@ public class Link {
 		return this.semanticConfidence;
 	}
 
+	private static boolean isCodeFile(FileInfo file) {
+		if (!file.filename.contains(".")) return false;
+		List<String> extensionsToIgnore = Arrays.asList("txt","md");
+		return extensionsToIgnore.stream().noneMatch(extension -> file.filename.endsWith("." + extension));
+	}
+
 	/**
 	 * For each modified file it calculates the suspect
 	 * 
@@ -251,7 +257,7 @@ public class Link {
 	 */
 	public void calculateSuspects(Git git, PrintWriter l) {
 		for (FileInfo fi : transaction.getFiles()) {
-			if (fi.filename.endsWith(".java")) {
+			if (isCodeFile(fi)) {
 					String diff = git.getDiff(transaction.getId(), fi.filename, l);
 					if (diff == null)
 						break;
